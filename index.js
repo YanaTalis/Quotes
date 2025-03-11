66ce5e1e0922d281cd00c961e8ab6b2c0bc4aa1a
YanaTalis <talis.yana98@gmail.com>
2025-03-11 20:58:23 +0200

    Show favorite quotes

--- index.js
+++ index.js
@@ -4,11 +4,48 @@ const quoteElement = document.getElementById('quote')
 const quoteAuthorElement = document.getElementById('author')
 const generateBtn = document.getElementById('generate-btn')
 
+const favoriteBtn = document.getElementById('favorite-btn')
+const favoritesContainer = document.getElementById('favorites-container')
+
+let currentQuoteIndex
+
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length)
-  const randomQuote = quotes[randomIndex]
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length)
+  const randomQuote = quotes[currentQuoteIndex]
+
   quoteElement.textContent = randomQuote.quote
   quoteAuthorElement.textContent = randomQuote.author
+
+  favoriteBtn.textContent = randomQuote.isFavorite ? 'Unfavorite' : 'Favorite'
+  favoriteBtn.style.display = 'inline-block'
+}
+
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex]
+  currentQuote.isFavorite = !currentQuote.isFavorite
+
+  favoriteBtn.textContent = currentQuote.isFavorite ? 'Unfavorite' : 'Favorite'
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement('div')
+    favoriteCard.classList.add('favorite-card')
+    favoriteCard.innerHTML = `
+    <p>${currentQuote.quote}</p>
+    <p class="author">${currentQuote.author}</p>
+    `
+    favoritesContainer.appendChild(favoriteCard)
+  } else {
+    //rrmove fav card (if its unfavorite)
+    const favoriteCards = document.querySelectorAll('.favorite-card')
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove()
+      }
+    })
+  }
 }
 
 generateBtn.addEventListener('click', generateRandomQuote)
+favoriteBtn.addEventListener('click', toggleFavorite)
+
+// generateRandomQuote()
